48d02f0b15dff1ac7f6da5aee1a1a4f6d3255600
Victor <leksymonit@gmail.com>
2023-03-06 12:53:01 +0300

    Добавление сервисов и конфигурации

--- src/main/java/com/example/logger/service/KafkaLogger.java
+++ src/main/java/com/example/logger/service/KafkaLogger.java
@@ -1,2 +1,19 @@
-package com.example.logger.service;public class KafkaLogger {
+package com.example.logger.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class KafkaLogger {
+
+  @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.groupId}")
+  public void processMessage(ConsumerRecord<String, String> consumerRecord) {
+    String value = consumerRecord.value();
+    log.info("Received value: " + value);
+  }
 }
